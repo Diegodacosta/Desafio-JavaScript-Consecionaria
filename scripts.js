653d365d26f0e9b4ce46a36a98ea608f19a133f8
Diego da Costa <diegodc169@gmail.com>
2022-08-08 23:52:55 -0300

    subida desafio eventos

--- scripts.js
+++ scripts.js
@@ -13,29 +13,53 @@ class Auto {
 
 const autos = [];
 
-let continuar = true;
-while (continuar) {
-    let ingreso = prompt("Ingrese los datos del auto: marca, modelo, anio, color, patente, precio..Todo separado por una barra diagonal (-) Ingresa X para salir");
+const botonEmpezar = document.getElementById('boton');
 
-    if (ingreso.toUpperCase() == "X") {
-        continuar = false;
-        break;
+
+function agregarAuto() {
+
+    let continuar = true;
+    while (continuar) {
+        let ingreso = prompt("Ingrese los datos del auto: marca, modelo, anio, color, patente, precio..Todo separado por una barra diagonal (-) Ingresa X para salir");
+
+        if (ingreso.toUpperCase() == "X") {
+            continuar = false;
+            break;
+        }
+
+        let datos = ingreso.split("-");
+        const auto = new Auto(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]);
+        autos.push(auto);
+        console.log(autos);
     }
-
-    let datos = ingreso.split("-");
-    const auto = new Auto(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]);
-    autos.push(auto);
-    console.log(autos);
 }
 
+botonEmpezar.addEventListener('click', agregarAuto);
 
 
-let criterio = prompt("Ingrese el criterio de busqueda: \n 1- marca (de la A a la Z)\n 2- modelo (de la A a la Z)\n 3- anio(mas viejo a mas nuevo)\n 4- color (de la A a la Z)\n 5- patente (de la A a la Z)");
+
+
+
+
+
+
+
+
+const botonBuscar = document.getElementById('botonBusqueda');
 
 function ordenar(criterio, array) {
+
+    let continuar1 = true;
+    while (continuar1) {
+        let ingreso = prompt("Ingrese el criterio de busqueda: \n 1- marca (de la A a la Z)\n 2- modelo (de la A a la Z)\n 3- anio(mas viejo a mas nuevo)\n 4- color (de la A a la Z)\n 5- patente (de la A a la Z)\n  Ingresa X para salir");
+        if (ingreso.toUpperCase() == "X") {
+            continuar = false;
+            break;
+        }
+    }
     let arrayOrdenado = array.slice(0);
 
-    switch (criterio) {
+    switch (ingreso) {
         case '1':
             return arrayOrdenado.sort((a, b) => a.marca.localeCompare(b.marca));
 
@@ -58,6 +82,11 @@ function ordenar(criterio, array) {
             alert("No es un criterio valido");
     }
 }
+
+botonBuscar.addEventListener('click', ordenar, mostrar);
+
+
+
 //  Funcion para mostrar//
 
 function mostrar(array) {
@@ -72,15 +101,26 @@ function mostrar(array) {
     alert(mostrar(ordenar(criterio, autos)));
 }
 
-let marcaElegido = prompt("Ingrese la marca elegida y le mostaremos los autos que tengan esa marca");
-let arrayFiltrado = autos.filter((auto) => auto.marca.toLowerCase().includes(marcaElegido.toLowerCase()));
+const botonMarca = document.getElementById('botonMarcas');
 
-if (arrayFiltrado.length == 0) {
-    alert("No hay autos de esa marca");
-} else {
-    const filtrados = arrayFiltrado.map(auto => auto.modelo);
-    alert("Los autos de la marca " + marcaElegido + " son: " + filtrados.join('\n'));
+function marca() {
+    let marcaElegido = prompt("Ingrese la marca elegida y le mostaremos los autos que tengan esa marca");
+    let arrayFiltrado = autos.filter((auto) => auto.marca.toLowerCase().includes(marcaElegido.toLowerCase()));
+
+    if (arrayFiltrado.length == 0) {
+        alert("No hay autos de esa marca");
+    } else {
+        const filtrados = arrayFiltrado.map(auto => auto.modelo);
+        alert("Los autos de la marca " + marcaElegido + " son: " + filtrados.join('\n'));
+    }
+
+
+
 }
+
+botonMarcas.addEventListener('click', marca);
+
+
 
 let colorElegido = prompt("Ingrese el color elegido y le mostaremos los autos que tengan ese color");
 let arrayFiltrado2 = autos.filter((auto) => auto.color.toLowerCase().includes(colorElegido.toLowerCase()));
